12676c5edc770a2b86d27becfb74765e04b9e089
Haroon Arshad <arshadharoon216@gmail.com>
2025-10-20 23:21:08 +0500

    feat: reorganize chat routes to include session management endpoints

--- src/routes/chat.route.ts
+++ src/routes/chat.route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware";
-import { getHistory, sendMessage } from "../controllers/chat.controller";
+import { createNewSession, getHistory, getSessionMessages, getSessions, sendMessage } from "../controllers/chat.controller";
 
 const router = Router();
 
 router.post('/', authMiddleware, sendMessage);
-router.get('/history', authMiddleware, getHistory);
+router.post('/new', authMiddleware, createNewSession);
+router.get('/sessions', authMiddleware, getSessions);
+router.get('/session/:sessionId', authMiddleware, getSessionMessages)
+// router.get('/history', authMiddleware, getHistory);
 
 export default router;
