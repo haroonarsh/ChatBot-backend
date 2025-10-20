41e7ce7082bffa93f68196152e432f2a21dca12e
Haroon Arshad <arshadharoon216@gmail.com>
2025-10-20 23:20:57 +0500

    feat: add createdAt and updatedAt fields to IChatSession interface

--- src/interfaces/IChatSession.ts
+++ src/interfaces/IChatSession.ts
@@ -2,6 +2,9 @@ import { Document, Types } from "mongoose";
 import { IMessage } from "./IMessage";
 
 export interface IChatSession extends Document {
+    _id: Types.ObjectId;
     user: Types.ObjectId; // User ID
     messages: IMessage[];
+    createdAt: Date;
+    updatedAt: Date;
 }
