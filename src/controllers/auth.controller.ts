905c96d0c4e28ac2485e770bf9e502dc500f824e
Haroon Arshad <arshadharoon216@gmail.com>
2025-10-26 00:54:57 +0500

    ensure token is set as a cookie in register and login functions

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,6 +7,7 @@ export const register = async (req: Request, res: Response) => {
     try {
         const { name, email, password } = req.body;
         const token = await authService.register(name, email, password);
+        res.cookie('token', token, { httpOnly: true });
         res.status(201).json({ token });
     } catch (error: any) {
         res.status(400).json({ message: error.message });
@@ -17,6 +18,8 @@ export const login = async (req: Request, res: Response) => {
     try {
         const { email, password } = req.body;
         const token = await authService.login(email, password);
+
+        res.cookie('token', token, { httpOnly: true }); 
         res.status(200).json({ token });
     } catch (error: any) {
         res.status(400).json({ message: error.message });
