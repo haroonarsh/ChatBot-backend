5d131b35fcbd091ab9b66a841451d650f765ac24
Haroon Arshad <arshadharoon216@gmail.com>
2025-11-01 22:48:56 +0500

    refactor app.ts for improved structure and readability

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,14 @@ dotenv.config({ path: __dirname + '/../.env' });
 const app: Express = express();
 
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: process.env.FRONTEND_URL,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true,
+    }
+));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
